23143a5081248339b4391409f212dfe1a01203a3
MuhammadFarhanA <farhan45778@gmail.com>
2023-09-21 11:46:43 +0500

    views added for stepper and competencies

--- src/app/modules/user-info/user-info.module.ts
+++ src/app/modules/user-info/user-info.module.ts
@@ -5,10 +5,7 @@ import { AddEditProfileComponent } from './add-edit-profile/add-edit-profile.com
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { MaterialModule } from 'src/app/material/material.module';
 import { AddEditBasicinformationComponent } from './add-edit-basicinformation/add-edit-basicinformation.component';
-import { AddEditEducationComponent } from './add-edit-education/add-edit-education.component';
-import { AddEditExperienceComponent } from './add-edit-experience/add-edit-experience.component';
-import { AddEditPortfolioComponent } from './add-edit-portfolio/add-edit-portfolio.component';
-import { AddEditSkillsComponent } from './add-edit-skills/add-edit-skills.component';
+
 import { ContactInfoComponent } from './contact-info/contact-info.component';
 import { AddressComponent } from './address/address.component';
 import { IdentificationComponent } from './identification/identification.component';
@@ -23,11 +20,7 @@ import { RouterModule } from '@angular/router';
   declarations: [
     UserInfoComponent,
     AddEditBasicinformationComponent,
-    AddEditEducationComponent,
-    AddEditExperienceComponent,
-    AddEditPortfolioComponent,
-    AddEditSkillsComponent,
-    AddEditProfileComponent,
+    
     ContactInfoComponent,
     AddressComponent,
     IdentificationComponent
@@ -40,11 +33,6 @@ import { RouterModule } from '@angular/router';
   ],
   exports:[
     AddEditBasicinformationComponent,
-    AddEditEducationComponent,
-    AddEditExperienceComponent,
-    AddEditPortfolioComponent,
-    AddEditSkillsComponent,
-    AddEditProfileComponent,
     ContactInfoComponent,
     AddressComponent,
     IdentificationComponent
