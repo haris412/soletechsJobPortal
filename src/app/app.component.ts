33fa66fb53ac0378388e6046e17dce46249855df
MuhammadFarhanA <farhan45778@gmail.com>
2023-10-21 01:26:00 +0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
 import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { routeTransition } from './app-animation';
 
@@ -15,7 +15,9 @@ export class AppComponent implements OnInit {
   public isPositionAbsolute: boolean = false;
   public o:any;
   constructor(
-    private router: Router
+    private router: Router,
+    private el: ElementRef, 
+    private renderer: Renderer2
   ) {
 
   }
@@ -32,8 +34,13 @@ export class AppComponent implements OnInit {
     if (!localStorage.getItem('token')) {
       this.router.navigate(['/']);
     }
+    this.changePrimaryColor('#005a9c');
   }
   getState(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData;
+  }
+  changePrimaryColor(newColor: string) {
+    const root = this.el.nativeElement.ownerDocument.documentElement;
+    console.log(this.renderer.setStyle(root, '--theme-color', newColor));
   }
 }
