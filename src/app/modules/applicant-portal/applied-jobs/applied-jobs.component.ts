7762bd984dcbf1b7f403939b1a3c66a763ab803d
haris412 <harissaeed214@gmail.com>
2024-01-04 01:06:09 +0500

    translation for applied jobs

--- src/app/modules/applicant-portal/applied-jobs/applied-jobs.component.ts
+++ src/app/modules/applicant-portal/applied-jobs/applied-jobs.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApplicantDataService } from '../services/applicant-shared.service';
+import { SharedService } from 'src/app/shared/services/shared.service';
+import { TranslationAlignmentService } from 'src/app/app-services/translation-alignment.service';
 
 @Component({
   selector: 'app-applied-jobs',
@@ -17,7 +19,10 @@ export class AppliedJobsComponent implements OnInit , OnChanges{
   public offerStage: boolean = false;
   public onBoardingStage: boolean = false;
 
-  constructor(private router: Router,private applicantService:ApplicantDataService) {
+  constructor(private router: Router,
+              private applicantService:ApplicantDataService,
+              public sharedService: SharedService,
+              public translationService: TranslationAlignmentService) {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -36,5 +41,22 @@ export class AppliedJobsComponent implements OnInit , OnChanges{
   }
   GoToJobs(){
     this.router.navigate(['/jobs']);
+  }
+
+  JobDetailLanguageChanges() {
+    // if (this.appliedJobs != null) {
+    //   if (this.translationService.isTranslate) {
+    //     this.selectedJob.Description = this.sharedService.jobDetail?.jobArabic ? this.sharedService.jobDetail?.jobArabic : this.sharedService.jobDetail?.Description;
+    //     this.selectedJob.JobLocation = this.sharedService.jobDetail?.jobLocationArabic ? this.sharedService.jobDetail?.jobLocationArabic : this.sharedService.jobDetail?.JobLocation;
+    //     this.selectedJob.JobAd = this.sharedService.jobDetail?.jobadTextArabic ? this.sharedService.jobDetail?.jobadTextArabic : this.sharedService.jobDetail?.JobAd;
+    //     this.selectedJob.Overview = this.sharedService.jobDetail?.OverviewArabic ? this.sharedService.jobDetail?.OverviewArabic : this.sharedService.jobDetail?.Overview;  
+    //     // this.selectedJob.Skills = this.sharedService.jobDetail?.SkillsArabicList ? this.sharedService.jobDetail?.SkillsArabicList : this.sharedService.jobDetail?.Skills;        
+    //     for(let skill of this.selectedJob.Skills?.parmRecruitmentSkillsList) {
+    //       skill.Skill = skill.skillsArabic ? skill.skillsArabic : skill.Skill;
+    //     }
+    //   } else {
+    //     this.selectedJob = this.sharedService.DeepCopyObject(this.sharedService.jobDetail);
+    //   }     
+    // }
   }
 }
